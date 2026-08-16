3a750f4e447968df8f4e43a39e6b8a8984981a08
ajayAnthwal <ajayanthwal600@gmail.com>
2026-08-16 21:38:16 +0530

    update car blink website

---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+﻿export const formatApiUrl = (rawUrl?: string): string => {
+  if (!rawUrl) return '';
+  let cleanUrl = rawUrl.trim().replace(/^["']|["']$/g, '');
+  if (cleanUrl && !cleanUrl.startsWith('http://') && !cleanUrl.startsWith('https://')) {
+    cleanUrl = `http://${cleanUrl}`;
+  }
+  return cleanUrl;
+};
